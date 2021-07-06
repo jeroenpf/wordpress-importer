f85a4a4d7eb2d15f062bda5311a06ff655d38b4f
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2021-07-06 14:05:54 +0700

    Base structure of the importer implemented

--- includes/Import.php
+++ includes/Import.php
@@ -2,17 +2,355 @@
 
 namespace Importer;
 
+use Opis\JsonSchema\Errors\ErrorFormatter;
+use Opis\JsonSchema\Validator;
+use WP_Error;
+use ZipArchive;
+use Exception;
+
 class Import {
 
+	const OPTION_PREFIX = 'wp_import_';
 
-	public function create() {
+	/**
+	 * An ordered list of type of objects
+	 */
+	const IMPORT_ORDER = array(
+		'posts',
+		'users',
+		'terms',
+	);
 
+	const SCHEMA_MAP = array(
+		'users' => 'https://wordpress.org/schema/user.json',
+		'posts' => 'https://wordpress.org/schema/post.json',
+		'meta'  => 'https://wordpress.org/schema/meta.json',
+		'terms' => 'https://wordpress.org/schema/term.json',
+	);
+
+	const LOG_WARNING = 'warning';
+	const LOG_ERROR   = 'error';
+
+	/**
+	 * @var ZipArchive
+	 */
+	protected $wxz;
+
+	/**
+	 * An index of JSON files to be indexed
+	 *
+	 * @var array
+	 */
+	protected $wxz_json_index = array();
+
+	/**
+	 * @var
+	 */
+	protected $start_time;
+
+	/**
+	 * Number of actions that have been processed during the current run.
+	 *
+	 * @var int
+	 */
+	protected $processed_counter = 0;
+
+	/**
+	 * Import constructor.
+	 *
+	 * @param $wxz_path
+	 *
+	 * @throws Exception
+	 */
+	public function __construct( $wxz_path ) {
+
+		$this->start_time = microtime( true );
+
+		$this->wxz = new ZipArchive();
+		$output    = $this->wxz->open( $wxz_path );
+
+		if ( true !== $output ) {
+			throw new Exception( sprintf( __( 'Error opening wxz-file. Error code %s', 'wordpress-importer' ), $output ) );
+		}
+
+		$this->validator = new Validator();
+		$resolver        = $this->validator->resolver();
+		$resolver->registerPrefix( 'https://wordpress.org/schema/', __DIR__ . '/../vendor/wordpress/wxz-tools/schema' );
 	}
-
 
 	public function run() {
 
+		// Index the WXZ.
+		$this->index_wxz();
 
+		while ( $this->can_proceed() ) {
+			$stage = get_option( self::OPTION_PREFIX . 'stage', 'start' );
+
+			switch ( $stage ) {
+
+				case 'start':
+					$this->pre_import();
+					break;
+
+				case 'objects':
+					$this->import_objects();
+					break;
+
+				case 'finalize':
+					break 2;
+
+			}
+		}
+
+	}
+
+	protected function pre_import() {
+
+		update_option( self::OPTION_PREFIX . 'stage', 'objects' );
+	}
+
+	/**
+	 * Import JSON objects from the WXZ
+	 */
+	protected function import_objects() {
+
+		while ( $this->can_proceed() ) {
+
+			// Get the next object to handle
+			$object = $this->get_next_object();
+
+			// If we ran out of objects, let the import proceed to the next stage because we are done here.
+			if ( null === $object ) {
+				update_option( self::OPTION_PREFIX . 'stage', 'finalize' );
+				break;
+			}
+
+			$zip_index = $this->wxz_json_index[ $object['type'] ][ $object['index'] ];
+			$file_path = $this->wxz->getNameIndex( $zip_index );
+			$data      = json_decode( $this->wxz->getFromIndex( $zip_index ), true );
+			$type      = self::IMPORT_ORDER[ $object['type'] ];
+
+			if ( false === $data ) {
+				// Cannot parse the json. Log an error.
+				$this->log( 'invalid-json', sprintf( 'Invalid JSON in %s', $file_path ) );
+				continue;
+			}
+
+			if ( ! $this->data_is_valid( $data, $type ) ) {
+				$this->log(
+					'schema-violation',
+					sprintf( 'The data in %s can not be validated against the schema.', $file_path )
+				);
+				continue;
+			}
+
+			// todo import
+			$this->import_object( $type, $data );
+
+			// Update the count of imported items
+			$this->processed_counter++;
+		}
+
+	}
+
+	protected function import_object( $type, $data ) {
+
+	}
+
+	/**
+	 * Validate data from JSON against a JSON schema.
+	 *
+	 * @param $data
+	 * @param $type
+	 *
+	 * @return bool
+	 */
+	protected function data_is_valid( $data, $type ) {
+		$schema = self::SCHEMA_MAP[ $type ];
+
+		try {
+			$result = $this->validator->validate( $data, $schema );
+		} catch ( Exception $e ) {
+			$this->log( 'validation-exception', $e->getMessage(), self::LOG_ERROR );
+			echo 'Exception: ' . $e->getMessage();
+			return null;
+		}
+
+		// todo perhaps log schema errors?
+
+		return $result->isValid();
+	}
+
+	/**
+	 * Get the type and index of the next JSON object.
+	 *
+	 * @return array|WP_Error|null
+	 */
+	protected function get_next_object() {
+
+		$option_name = self::OPTION_PREFIX . 'import_state';
+		$locked      = $this->obtain_state_lock();
+
+		if ( $locked instanceof WP_Error ) {
+			return $locked;
+		}
+
+		$state = get_option(
+			$option_name,
+			array(
+				'type'  => 0,
+				'index' => -1,
+			)
+		);
+
+		$state['index']++;
+
+		if ( ! isset( $this->wxz_json_index[ $state['type'] ][ $state['index'] ] ) ) {
+			$state['type']  = $this->get_next_type( $state['type'] );
+			$state['index'] = 0;
+		}
+
+		// If the type is false, there are no more objects to process.
+		if ( false === $state['type'] ) {
+			$state = null;
+			delete_option( $option_name );
+		} else {
+			update_option( $option_name, $state );
+		}
+
+		$this->release_state_lock();
+
+		return $state;
+
+	}
+
+	/**
+	 * Get the next type of json object to import.
+	 *
+	 * @param int $current_type The current type.
+	 *
+	 * @return false|int
+	 */
+	protected function get_next_type( $current_type ) {
+
+		$next_type = $current_type + 1;
+
+		if ( ! array_key_exists( $next_type, self::IMPORT_ORDER ) ) {
+			return false;
+		}
+
+		// If the type has any indexed objects, return them, otherwise proceed to the next type.
+		return empty( $this->wxz_json_index[ $next_type ] )
+			? $this->get_next_type( $next_type )
+			: $next_type;
+	}
+
+	/**
+	 * @return bool|WP_Error
+	 */
+	protected function obtain_state_lock() {
+
+		$lock_name = self::OPTION_PREFIX . 'state_lock';
+		$tries     = 0;
+		$lock      = false;
+
+		while ( ! $lock && $tries < 10 ) {
+
+			$tries++;
+
+			$lock = $this->insert_state_lock( $lock_name );
+
+			// Check for an expired lock
+			if ( ! $lock && get_option( $lock_name ) > time() - 5 ) {
+				update_option( $lock_name, time() );
+				$lock = true;
+			}
+
+			usleep( 100 );
+		}
+
+		if ( ! $lock ) {
+			return new \WP_Error( 'no_state_lock', __( 'Can not obtain state lock' ) );
+		}
+
+		return true;
+
+	}
+
+	protected function insert_state_lock( $lock_name ) {
+		global $wpdb;
+
+		$lock_insert = $wpdb->prepare(
+			"
+			INSERT IGNORE INTO `$wpdb->options` ( `option_name`, `option_value`, `autoload` )
+			VALUES (%s, %s, 'no') /* LOCK */",
+			$lock_name,
+			time()
+		);
+
+		return $wpdb->query( $lock_insert );
+	}
+
+
+	protected function release_state_lock() {
+		$lock_name = self::OPTION_PREFIX . 'state_lock';
+
+		delete_option( $lock_name );
+	}
+
+	/**
+	 * Checks memory and execution time limits.
+	 *
+	 * @todo Implement memory limits here.
+	 */
+	protected function can_proceed() {
+
+		return ! $this->exceeding_time_limit();
+	}
+
+	/**
+	 * @return bool
+	 * @todo Improve this
+	 */
+	protected function exceeding_time_limit() {
+
+		$time_limit = (int) ini_get( 'max_execution_time' );
+
+		$total_time = microtime( true ) - $this->start_time;
+
+		if ( 0 === $time_limit || 0 === $this->processed_counter ) {
+			return false;
+		}
+
+		$time_per_action = $total_time / $this->processed_counter;
+
+		// There needs to be time to run 4 more actions based on average time.
+		return $time_per_action * ( $this->processed_counter + 4 ) > $time_limit;
+
+	}
+
+	protected function log( $code, $message, $level = self::LOG_WARNING ) {
+		printf( "[%s][%s] %s\n", $level, $code, $message );
+	}
+
+	/**
+	 * Indexes the WXZ json files.
+	 */
+	protected function index_wxz() {
+		$index = array();
+		for ( $i = 0; $i < $this->wxz->numFiles; $i++ ) {
+
+			$file = $this->wxz->getNameIndex( $i );
+			$type = dirname( $file );
+
+			if ( ! in_array( $type, self::IMPORT_ORDER, true ) || pathinfo( $file, PATHINFO_EXTENSION ) !== 'json' ) {
+				continue;
+			}
+
+			$index[ array_search( $type, self::IMPORT_ORDER ) ][] = $i;
+		}
+
+		$this->wxz_json_index = $index;
 	}
 
 
--- phpunit/tests/importer.php
+++ phpunit/tests/importer.php
@@ -1,10 +1,30 @@
 <?php
 
+use Importer\Import;
+
 class Tests_Importer extends WP_UnitTestCase {
 
 	public function test_sample() {
 
 		$this->assertTrue( true );
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function test_can_create_import_instance_with_valid_zip() {
+
+		$this->expectNotToPerformAssertions();
+		$import = new Import( DIR_TESTDATA_WORDPRESS_IMPORTER . '/wordpress-export.zip' );
+	}
+
+	public function test_can_index_zip_file() {
+
+		$import = new Import( DIR_TESTDATA_WORDPRESS_IMPORTER . '/wordpress-export.zip' );
+
+		$import->run();
+
+
 	}
 
 }
